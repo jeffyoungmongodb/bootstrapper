140038f463d0310a1f98465287695a489a38ea2f
Jeff Young <jeff.young@mongodb.com>
2016-12-06 13:19:53 -0500

    Added recursion.

--- samp/sqrt/sqrt.go
+++ samp/sqrt/sqrt.go
@@ -23,9 +23,45 @@ func main() {
   }
 
   fsqrtval := square_root(fval)
-  fmt.Printf("square root of %0.8f is %0.8f\n",fval,fsqrtval)
+  fmt.Printf("iterative square root of %0.8f is %0.8f\n",fval,fsqrtval)
+  
+  fsqrtval_recurse := square_root_r(fval,0,0)
+  fmt.Printf("recursive square root of %0.8f is %0.8f\n",fval,fsqrtval_recurse)
 
 }
+
+func square_root_r(val float64, hi float64, lo float64) (float64) {
+
+  var MARGIN float64 = 0.000000001 
+  if  hi <= 0.0 {  
+    hi = val
+    if val < 1 {
+      hi = 1
+    }
+  }
+
+  if  lo <= 0.0 {
+    lo = 0
+    if val >= 1 {
+      lo = 1
+    }
+  }
+ 
+  var guess float64 = (hi + lo) / 2
+  var chk float64 = guess * guess
+  if  math.Abs( chk - val ) <= MARGIN {
+    return guess
+  }
+  if chk > val {
+    hi = guess
+  } else {
+    lo = guess
+  }
+  
+  return square_root_r(val,hi,lo)
+
+}
+
 
 func square_root(val float64) (float64) {
 
