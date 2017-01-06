2903c3a95f7d4510aad403246249ad0f07655933
Jeff Young <jeff.young@mongodb.com>
2017-01-06 14:13:52 -0500

    Latest updated.

--- samp/force/mtse.pl
+++ samp/force/mtse.pl
@@ -1,7 +1,12 @@
 #!/usr/bin/env perl
 use strict;
 
+
+
 sub util::stdout($);
+
+$main::DEBUG = 0;
+
 die "Cannot use JSON, try as root : 'cpan install json'"
   unless json::can_use();
 
@@ -12,14 +17,26 @@ force_cli::login()
   unless force_cli::is_logged_in();
 
 my $mtseConf = mtse::load_config();
+
 my $cache = mtse::load_cache($mtseConf);
+mtse::summarize_tse_data($cache);
 
 json::print($cache, 'CACHE');
 
 
+mtse::print_summary_report($cache);
+
+
+
+
 exit(0);
 
+#use DateTime; my $dt = DateTime->now; my $x = $dt->iso8601(); print "X: $x\n";
 #######
+
+
+
+
 
 
 package util;
@@ -53,6 +70,8 @@ sub json::decode
 {
   my $json = shift;
   require JSON;
+
+  print "JSON: \n{{\n$json\n}}\n" if $main::DEBUG;
   my $res = JSON::decode_json($json);
   return $res;
 }
@@ -106,6 +125,8 @@ sub json::encode
   my $str = $json->encode($ds);
   return $str;
 }
+
+
 
 package force_cli;
 
@@ -168,11 +189,28 @@ sub force_cli::is_logged_in
 sub force_cli::query
 {
   my $q = shift;
-  my ($ex,$rc,$ret) = force_cli::exec("query \"$q\" --format:json");
-  my $d = json::decode($ret);
-  return $d;
+
+  my $tries = 2;
+
+  while ($tries--)
+  {
+
+    my ($ex,$rc,$ret) = force_cli::exec("query \"$q\" --format:json");
+    if ($ret =~ /^ERROR/)
+    {
+      util::stdout "retrying... after force cli error ($ret)";
+      next;
+    }
+
+    my $d = json::decode($ret);
+
+    return $d;
+  }
+
+  return undef;
 }
 
+####
 
 package mtse;
 
@@ -253,18 +291,139 @@ sub mtse::get_tse_data
   $data ->{Name} = $nm;
 
   my 
-  $soql = "Select Case.OwnerId,Case.Status,Case.CaseNumber,AccountId,CreatedDate,ClosedDate,Comment_Count__c,Components__c from Case WHere Case.OwnerId = '$id'";
+  $soql = "Select Case.OwnerId,Case.Status,Case.CaseNumber,Id,Subject,AccountId,CreatedDate,ClosedDate,Comment_Count__c,Components__c from Case WHere Case.OwnerId = '$id'";
  
   util::stdout "querying cases for $nm";
   my $CaseList = force_cli::query($soql);
   $data->{CaseList} = $CaseList;
 
-  $soql = "Select CreatedDate,Is_Published__c,Case__c,Id,CreatedById,Created_By_Name__c,Is_FTS_Comment__c,Name from Case_Comment__c where CreatedById = '$id'" ;
+  $soql = "Select CreatedDate,Is_Published__c,Case__c,Id,CreatedById,Created_By_Name__c,Is_FTS_Comment__c,Name from Case_Comment__c where CreatedById = '$id' order by CreatedDate DESC" ;
   util::stdout "querying comments for $nm";
   my $CommentList = force_cli::query($soql);
   $data->{CommentList}  = $CommentList;
 
+  $soql = "SELECT Id, CreatedDate, OwnerId, Markdown_Text__c FROM Review_Comment__c where OwnerId = '$id' ORDER BY CreatedDate DESC";
+
+  util::stdout "querying reviews by $nm";
+  my $ReviewList = force_cli::query($soql);
+  $data->{ReviewList} = $ReviewList;
+
+
   return $data;
 
+}
+
+
+
+sub mtse::is_closed_status
+{
+  my $val = shift;
+
+  return 1 if $val eq 'Resolved';
+  return 1 if $val eq 'Closed';
+  return 0 ;
+}
+
+
+sub mtse::print_summary_report
+{
+  my $cache = shift;
+
+
+  print "BEGIN TSE SUMMARY REPORT.\n";
+
+  while (my ($k,$v) = each %{$cache->{tses}})
+  {
+    print "TSE: $k TotalCases=$v->{Summary}->{CaseCount}, TotalComments=$v->{Summary}->{CommentCount} \n";
+    print "  Open Cases:\n";
+    foreach my $x (@{$v->{Summary}->{OpenCases}}) { print "    $x\n"; }  
+    print "  Latest Comments:\n";
+    foreach my $y (@{$v->{Summary}->{LatestComments}}) { print "    $y\n"; }  
+
+    print "\n";
+  }
+
+
+  print "END TSE SUMMARY REPORT.\n";
+}
+
+sub mtse::summarize_tse_data
+{
+  my $cache = shift;
+
+
+  while (my ($k,$v) = each %{$cache->{tses}})
+  {
+
+    my $CaseHash = {};
+
+    my $caseList = $v->{Data}->{CaseList};
+    my $commentList = $v->{Data}->{CommentList};
+    my $reviewList = $v->{Data}->{ReviewList};
+
+    my $case_cnt = 0;
+    my $comment_cnt = 0;
+    my $review_cnt = 0;
+
+
+    foreach my $case (@$caseList) 
+    { 
+      $case_cnt++; 
+      if ( ! mtse::is_closed_status($case->{Status})) 
+      {
+        $v->{Summary}->{OpenCases} = [] unless defined $v->{Summary}->{OpenCases};
+        push(@{$v->{Summary}->{OpenCases}}, "$case->{CaseNumber} : $case->{Status} : $case->{Subject}");
+        $CaseHash->{$case->{Id}} = $case;
+      }
+
+    }   
+   
+
+     
+    foreach my $comment (@$commentList) 
+    { 
+      $comment_cnt++; 
+        
+      $v->{Summary}->{LatestComments} = [] unless defined $v->{Summary}->{LatestComments};
+     
+
+      if ($comment_cnt < 10)
+      {
+        my $case = $CaseHash->{$comment->{'Case__c'}};
+        if (! defined $case)
+        {
+        
+          ##HERE 
+          my 
+          $soql = "Select Case.OwnerId,Case.Status,Case.CaseNumber,Id,Subject,AccountId,CreatedDate,ClosedDate,Comment_Count__c,Components__c from Case Where Id = '$comment->{'Case__c'}'";
+
+          my $retrvCase = force_cli::query($soql);
+          $case = pop (@{$retrvCase});
+        }
+
+        my $str = "$case->{CaseNumber} : $comment->{Name} : $comment->{CreatedDate} : $case->{Subject}";
+
+        push(@{$v->{Summary}->{LatestComments}},$str)
+          unless $comment_cnt > 10;
+      }
+      
+    }   
+
+    foreach my $review (@$reviewList) 
+    { 
+      $review_cnt++; 
+      $v->{Summary}->{LatestReviews} = [] unless defined $v->{Summary}->{LatestReviews};
+
+    }
+
+    $v->{Summary}->{CaseCount} = $case_cnt; 
+    $v->{Summary}->{CommentCount} = $comment_cnt; 
+    $v->{Summary}->{ReviewCount} = $review_cnt; 
+
+  }
   
 }
+
+
+
+
